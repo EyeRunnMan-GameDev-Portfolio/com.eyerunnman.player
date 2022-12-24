6ccb791879b447a4ba25eae917eb68879922f7f6
Karan Batra <Karanbtra007@gmail.com>
2022-12-24 11:57:50 +0530

    fix: replaced placeholder variables like `org-name` and `package-name`

---
+++ Runtime/PlayerImportTest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.eyerunnman.player
+{
+    public class PlayerImportTest : MonoBehaviour
+    {
+        public float TestFloat = 0;
+        
+    }
+}
